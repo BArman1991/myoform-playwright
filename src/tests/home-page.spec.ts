9e7f2f4e97887eff06d9b5f31777d28223dd67d1
BArman1991 <armanbelikov@gmail.com>
2025-02-17 02:25:41 +0200

    refactor

--- src/tests/home-page.spec.ts
+++ src/tests/home-page.spec.ts
@@ -1,10 +1,10 @@
-import { test, expect } from '@playwright/test';
-import { HomePage } from '../pages/landing-page';
-import dotenv from 'dotenv';
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../pages/landing-page";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-test.describe('Home Page Tests', () => {
+test.describe("Home Page Tests", () => {
   let homePage: HomePage;
 
   test.beforeAll(async ({ browser }) => {
@@ -14,23 +14,26 @@ test.describe('Home Page Tests', () => {
     await homePage.goto();
   });
 
-  test('Open home page', async () => {
+  test("Open home page", async () => {
     const headerText = await homePage.getHeaderText();
     expect(headerText).toMatch(/Welcome Back/);
 
     const spanLogIn = await homePage.getSpanLogInIsShownUnderH2();
     expect(spanLogIn).toMatch(/Log in to your account/);
-
   });
 
-  test('Log In', async () => {
+  test("Log In", async () => {
     const username = process.env.USERNAME;
     const password = process.env.PASSWORD;
     if (!username || !password) {
-      throw new Error('USERNAME and PASSWORD environment variables must be set');
+      throw new Error(
+        "USERNAME and PASSWORD environment variables must be set"
+      );
     }
     await homePage.login(username, password);
-    const welcomeBanner = homePage.page.locator('[class*=-greetings]:has-text("Hi Arman!")');
+    const welcomeBanner = homePage.page.locator(
+      '[class*=-greetings]:has-text("Hi Arman!")'
+    );
     await expect(welcomeBanner).toBeVisible();
   });
 });
---
+++ src/tests/login-page.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/login-page";
+import dotenv from "dotenv";
+
+test("User should be able to log in", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const username = process.env.USERNAME;
+  const password = process.env.PASSWORD;
+  if (!username || !password) {
+    throw new Error("USERNAME and PASSWORD environment variables must be set");
+  }
+  await loginPage.login(username, password);
+
+  await expect(page).toHaveURL("/dashboard");
+});
