4c0fb3470afab0bac25692f7634699c45f57a344
BArman1991 <armanbelikov@gmail.com>
2025-02-21 18:52:27 +0200

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,27 +1,19 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./src/tests",
   use: {
-    baseURL: 'https://dev-app.myoform.io/sign-in',
-    headless: true,
-    viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    baseURL: "https://dev-app.myoform.io/sign-in",
+    headless: false,
   },
+  reporter: [
+    ["html", { open: "never" }],
+    ["json", { outputFile: "playwright-report.json" }],
+  ],
   projects: [
     {
-      name: 'chromium',
-      use: { browserName: 'chromium' },
-    },
-    {
-      name: 'firefox',
-      use: { browserName: 'firefox' },
-    },
-    {
-      name: 'webkit',
-      use: { browserName: 'webkit' },
+      name: "chromium",
+      use: { browserName: "chromium" },
     },
   ],
 });
